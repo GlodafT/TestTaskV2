996bda568da453bd0f2debe107797c9eba2ab54c
GlodafT <nol-tup@ya.ru>
2020-12-16 16:09:05 +0300

    CLEAN (убрал лишнее)

--- testV2.playground/Contents.swift
+++ testV2.playground/Contents.swift
@@ -1,32 +1,17 @@
 import UIKit
 
-//var str = "Hello, playground"
-//
-
-
-
-
 //Создайте перечисление CocktailName со следующими безалкогольными коктейлями:
-
 //orangeMojito (состав: orange, mint, lime, spakrling water)
-
 //cranberrySparkler (состав: cranberry, sparkling water, lime, orange)
-
 //pinkLemonade (состав: lemon, orange, raspberry, mint, cold water)
-
 //sangriaPunch (состав: non-alchocol red wine, orange, lemon, lime, cold water)
-
 //Создайте перечисление Ingridients, в котором будут перечислены все ингриденты, нужные для приготовления коктейлей из пункта 1.
 //Задайте каждому case связанное значение с плавающей точкой, равное стоимости ингридента (стоимость должна быть уникальной)
-
 //Создайте словарь cocktails, в котором ключ — имя коктейля, а значение — массив типа Ingridients, который содержит коктейль.
-
 //На основе словаря cocktails, создайте новый словарь cocktailPrices, в котором ключ - имя коктейля, а значение - сумма за коктейль (складывается из стоимости его ингридиентов).
-
 //Добавление значений в словарь происходит в цикле. Также, как и вычисление стоимости коктейля).
 //Выведите элементы словаря в консоль в формате: "Цена за имяКоктейля - n".
 //Посчитайте среднюю сумму коктейля в коктейльной карте (сумма за каждый коктейль, делённая на количество коктейлей)
-
 //Создайте случайное имя коктейля. Если не удалось, то установите дефолтный коктейль - sangriaPunch
 //Создайте тьюпл client с 2 аргументами:
 //имя коктейля, который он хочет заказать
@@ -54,11 +39,12 @@ enum Ingridients: Double {
 }
 
 var cocktails: [CocktailName: [Ingridients]] = [.orangeMojito: [.orange, .mint, .lime, .spakrlingWater],
-                                                .cranberrySparkler: [.cranberry, .spakrlingWater, .lime, .orange], .pinkLemonade: [.lemon, .orange, .raspberry, .mint, .coldWater], .sangriaPunch: [.nonAlchocolRedWine, .orange, .lemon, .lime, .coldWater]]
+                                                .cranberrySparkler: [.cranberry, .spakrlingWater, .lime, .orange],
+                                                .pinkLemonade: [.lemon, .orange, .raspberry, .mint, .coldWater],
+                                                .sangriaPunch: [.nonAlchocolRedWine, .orange, .lemon, .lime, .coldWater]]
 
 
 var cocktailPrices: [CocktailName: Double] = [:]
-
 var summOfCocktails: Double = 0
 var everageOfCocktails: Double = 0
 
@@ -68,11 +54,10 @@ for (coctail, ingridients) in cocktails {
     var priceOfIngridients: Double = 0
     for price in ingridients {
          priceOfIngridients += price.rawValue
-    
     }
-//    print(priceOfIngridients)
     cocktailPrices.updateValue(priceOfIngridients, forKey: coctail)
 }
+
 
 for PrintPriceOfCockteils in cocktailPrices {
     print("Price of cocktail \(PrintPriceOfCockteils.key) - \(PrintPriceOfCockteils.value)")
@@ -80,27 +65,18 @@ for PrintPriceOfCockteils in cocktailPrices {
     everageOfCocktails = summOfCocktails / Double(cocktailPrices.count)
 }
 
-//print(everageOfCocktails)
 var randomCocktail = cocktailPrices.keys.randomElement() ?? .sangriaPunch
-//if randomCocktail == nil {
-//    randomCocktail = .sangriaPunch
-//}
+
 let client = (CocktailName.sangriaPunch, Double(25))
     
-
-
 for (cocktail1, price) in cocktailPrices {
     switch cocktail1 {
-//    case _ where client.0 == cocktail1 :
-//        if client.1 >= price {
-//            print("Keep your \(cocktail1)")
-//        } else {
-//            print("Need more gold, choose something else")
-//        }
+
     case _ where client.0 == cocktail1 && client.1 >= price :
         print()
         print("Keep your \(cocktail1)")
         break
+        
     case _ where client.0 == cocktail1 && client.1 < price :
         print()
         print("You need more gold, for a \(cocktail1)")
@@ -109,50 +85,8 @@ for (cocktail1, price) in cocktailPrices {
                 print()
                 print("You can buy \(cocktail123)")
             }
-           // print(cocktail123)
         }
-        
-//    case _ where  client.1 >= price :
-//        print(cocktail1)
-       
-        
     default:
        ()
     }
 }
-
-
-//    switch cocktailPrices {
-//    case _ where client.0 == cocktailPrices[.cranberrySparkler] :
-//        print(1)
-//    default:
-//        <#code#>
-//    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//cocktails[.cranberrySparkler] = [Ingridients.orange, Ingridients.mint, Ingridients.lime, Ingridients.spakrlingWater]
-//[.orangeMojito: [.orange, .mint, .lime, .spakrlingWater]]
-
-//cocktails= [.cranberrySparkler: [.cranberry, .spakrlingWater, .lime, .orange]]
-//var cocktails: [CocktailName: [Ingridients]] = [.pinkLemonade: [.lemon, .orange, .raspberry, .mint, .coldWater]]
-//var cocktails: [CocktailName: [Ingridients]] = [.sangriaPunch: [.nonAlchocolRedWine, .orange, .lemon, .lime, .coldWater]]
-
